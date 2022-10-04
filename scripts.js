ca9da4180e3dc73cdf419bebcb1f96514357848e
scottzyang <scottzyang@gmail.com>
2022-10-04 13:17:50 -0500

    Initial commit

--- scripts.js
+++ scripts.js
@@ -1,29 +1,36 @@
 // DOM references
-const widthInput = document.querySelector("#width-input");
-const heightInput = document.querySelector("#height-input");
-const colorInput = document.querySelector("#color-input");
-const displayWidth = document.querySelector("#display-width");
-const displayHeight = document.querySelector("#display-height");
-const displayColor = document.querySelector("#display-color");
-const colorBox = document.getElementById("color-input");
+const widthInput = document.querySelector("#width-input"); //
+const heightInput = document.querySelector("#height-input"); //
+const displayWidth = document.querySelector("#display-width"); //
+const displayHeight = document.querySelector("#display-height"); //
+const colorInput = document.querySelector("#color-input"); //
+const hexDisplayColor = document.querySelector("#display-color");
+const colorBox = document.querySelector("#color-box");
+
+const updateColorBox = (event) => {
+  const newColor = colorInput.value;
+  hexDisplayColor.innerHTML = newColor;
+  colorInput.style.backgroundColor = newColor;
+  colorBox.style.backgroundColor = newColor;
+};
 
 const updateDisplay = (event) => {
   // add logic to account for negative values
   const width = widthInput.value;
   const height = heightInput.value;
-  const color = colorInput.value;
 
   width > 0
     ? (displayWidth.innerHTML = `${width}px`)
     : (displayWidth.innerHTML = "Input Valid Width");
+  colorBox.style.width = `${width}px`;
   height > 0
     ? (displayHeight.innerHTML = `${height}px`)
     : (displayHeight.innerHTML = "Input Valid Height");
-  displayColor.innerHTML = colorInput.value;
+  colorBox.style.height = `${height}px`;
 };
 
 widthInput.addEventListener("input", updateDisplay);
 heightInput.addEventListener("input", updateDisplay);
-colorInput.addEventListener("input", updateDisplay);
+colorInput.addEventListener("input", updateColorBox);
 
 // need to target and set style of background of top right/bottom right color box.
